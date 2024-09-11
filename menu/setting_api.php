19658b57fbcb6eb46170ddf0baf5d7949bbddc50
sablu1234 <150502592+sablu1234@users.noreply.github.com>
2024-09-11 12:42:34 +0600

    plugin

--- menu/setting_api.php
+++ menu/setting_api.php
@@ -37,9 +37,71 @@ function github_callback(){
 }
 
 
+/*==============================
+*Custom top menu with custom api
+=============================*/
+//Top label menu
+if (!function_exists('sh_top_menu')) {
+    function sh_top_menu() {
+        add_menu_page(
+            'Submit your Page Title', // page title
+            'Submit your data', // menu title
+            'manage_options', // capability
+            'top_menu_slug', // slug
+            'sh_top_callback', // callback function
+            'dashicons-games', // dashicon
+            20
+        );
+    }
+
+	function sh_top_callback(){
+		?>
+		<h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
+		<form action="options.php" method="post">
+			<?php 
+			settings_fields('option_group');
+			do_settings_sections('custome_sections_name');
+			submit_button(__('save data','textdomain'));
+			?>
+		</form>
+		<?php
+	}
+}
+
+add_action('admin_menu', 'sh_top_menu');
 
 
+function option_group_function(){
+	//Registration waith database
+	register_setting('option_group','input_data');
+	//Add sections
+	add_settings_section(
+		'section_id', //section id
+		'Your all data', //section title
+		'custom_setting_callback', //callback
+		'custome_sections_name', //custom section page name
+	);
+	//add section field
+	add_settings_field(
+		'add_field_id', //field id
+		'Enter Your sell Number', //field title
+		'sh_field_callback', //callcack
+		'custome_sections_name', //page
+		'section_id',
+	);
 
+}
+add_action('admin_init','option_group_function');
+
+function custom_setting_callback(){
+	echo "Enter your all data";
+}
+
+function sh_field_callback(){
+	?>
+	<input type="text" name="input_data" value="<?php echo get_option('input_data');?>">
+	<?php
+}
 
 
 
